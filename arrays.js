b12a67b930bd640f07bf4d7bccf5079db3796e1c
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-02-09 19:19:03 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles" ];
 function addElementToBeginningOfArray(chocolateBars, "foo"){
-  return [candyString, ...chocolateBars];
+  return ["foo", ...chocolateBars];
 }
 
  
