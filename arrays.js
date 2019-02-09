73077da53f447547f88defd35715c888a99f665b
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-02-09 19:36:19 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -13,3 +13,4 @@ function destructivelyAddElementToEndOfArray(array, element) { array.push(elemen
 function accessElementInArray(array, index) { return array[index]}
 function destructivelyRemoveElementFromBeginningOfArray(array) {array.shift()
 return array}
+function removeElementFromBeginningOfArray(array) { return 
