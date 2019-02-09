363d3f81251f3c77e09d3c0b1f88be71c867f213
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-02-09 20:06:28 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -16,4 +16,4 @@ return array}
 function removeElementFromBeginningOfArray(array) { return array.slice(1)}
 function destructivelyRemoveElementFromEndOfArray(array) {array.pop() 
 return array}
-function removeElementFromEndOfArray(array) { array.slice(0, array.length -1)}
+function removeElementFromEndOfArray(array) {return array.slice(0, array.length -1)}
