826ca5e7cd26c6ea9efa1ffd2f5418d479cb4ea6
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-02-09 19:31:27 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -7,5 +7,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) { array.unshi
 }
  
 function addElementToEndOfArray(array, element) {return [...array, element]}
-function destructivelyAddElementToEndOfArray(array, element) {return 
+function destructivelyAddElementToEndOfArray(array, element) { array.push(element)
+  return array
+}
 function accessElementInArray(array, index) { return array[index]}
